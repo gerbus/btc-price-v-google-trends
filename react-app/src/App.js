7e2d8e562207a923344da438a7079410430f9c50
gerbus <gerbus3@gmail.com>
2017-11-13 09:52:21 -0800

    got start date working, but chart refreshes twice

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,36 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Chart from 'chart.js';
+import ChartJs from 'chart.js';
 import dateformat from 'dateformat';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+ 
+import 'react-datepicker/dist/react-datepicker.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    
-    // Chart.js defaults
-    Chart.defaults.global.elements.point.radius = 1;
-    Chart.scaleService.updateScaleDefaults('logarithmic', {
-      ticks: {
-        callback: function(tick, index, ticks) {
-          return tick.toLocaleString();
-        }
-      }
-    });
-    
+
     // Class members
     this.timeout = null;
     this.bitcoinLabels = [];
     this.bitcoinData = [];
     this.searchLabels = [];
     this.searchData = [];
-    this.startTime = new Date("2013-01-01"),
-    this.endTime = new Date(),
+    //this.startTime = moment("2013-01-01","YYYY-MM-DD");
+    this.endTime = moment();
+    this.searchKeyword = "bitcoin bubble";
     
     this.handleKeyword = this.handleKeyword.bind(this);
+    this.handleStartDateChange = this.handleStartDateChange.bind(this);
     
     // React component state
     this.state = {
+      startTime: moment("2013-01-01","YYYY-MM-DD"),
       chartData: {
         labels: [],
         datasets: [{ 
@@ -47,75 +44,6 @@ class App extends Component {
         }]
       }
     };
-    this.getBitcoinData(false);
-    this.getSearchData("bitcoin bubble");
-  }
-  getBitcoinData(update) {
-    // Data from Coindesk (see /routes/coindesk.js)
-    var endpointDev = "/coindesk/" + this.startTime.getTime() + "-" + this.endTime.getTime();
-    var endpoint = "/api/coindesk/" + this.startTime.getTime() + "-" + this.endTime.getTime();
-    
-    fetch(endpoint)
-    .then(response => response.json())
-    .then(data => {
-      //console.log(data);
-      this.bitcoinLabels = [];
-      this.bitcoinData = [];
-      for (var key in data.bpi) {
-        this.bitcoinLabels.push(key);
-        this.bitcoinData.push(data.bpi[key]);
-      }
-      if (update) {
-        this.updateChartData("");
-      }
-    })
-    .catch(err => console.log("Fetch error: " + err))
-  }
-  getSearchData(keyword) {
-    // Data from Google Trends (see /routes/googletrendsapi.js)
-    var endpointDev = "/googletrendsapi/" + encodeURIComponent(keyword) + "/" + this.startTime.getTime() + "-" + this.endTime.getTime();
-    var endpoint = "/api/googletrendsapi/" + encodeURIComponent(keyword) + "/" + this.startTime.getTime() + "-" + this.endTime.getTime();
-    
-    fetch(endpoint)
-    .then(response => response.json())
-    .then(data => {
-      //console.log(data);
-      this.searchLabels = [];
-      this.searchData = [];
-      data.default.timelineData.map(item => {
-        let sunday = new Date(item.formattedAxisTime);
-        for (let d = 0; d < 7; d++) {
-          let day = new Date(sunday);
-          day.setDate(day.getDate() + d);
-          this.searchLabels.push(dateformat(day,"isoDate"));
-          this.searchData.push(item.value[0]);
-        }
-      });
-      //console.log(this.searchLabels);
-      this.updateChartData(keyword);
-    })
-    .catch(err => console.log("Fetch error: " + err))
-  }
-  updateChartData(keyword) {
-    let chartData = this.state.chartData;
-    chartData.labels = this.bitcoinLabels; // Use the dates from coindesk data as a base
-    chartData.datasets[0].data = this.bitcoinData;
-    chartData.datasets[1].label = '"' + keyword + '"';
-    chartData.datasets[1].data = [];
-    
-    // Find values for each of the dates from coindesk data
-    this.bitcoinLabels.map(date => {
-      let match = false;
-      for (let i = 0; i < this.searchLabels.length; i++) {
-        if (this.searchLabels[i] == date) {
-          chartData.datasets[1].data.push(this.searchData[i]);
-          match = true;
-          break;
-        }
-      }
-      if (!match) { chartData.datasets[1].data.push(null); }
-    });
-    this.setState({ chartData: chartData });
   }
   render() {
     // See here for chart.js with react sample: https://tuespetre.github.io/react/responsive/2015/04/17/using-responsive-chart-js-charts-within-react.html
@@ -139,22 +67,38 @@ class App extends Component {
         
         <div className="container">
           <div className="row">
-          <div className="col-md-3">
-            <p>Compare the price of Bitcoin (log scale, left axis) to the occurrence of a google search (right axis, peak set to 100)</p> 
-            <div className="input-group">
-              <span className="input-group-addon">Search Keyword</span>
-              <input className="form-control" defaultValue="bitcoin bubble" onChange={event => this.handleKeyword(event.target.value)} />
+            <div className="col-md-4">
+              <p>Compare the price of Bitcoin (log scale, left axis) to the occurrence of a google search (right axis, peak set to 100)</p> 
+              <div className="input-group">
+                <span className="input-group-addon">Search Keyword</span>
+                <input className="form-control" defaultValue="bitcoin bubble" onChange={event => this.handleKeyword(event.target.value)} />
+              </div>
             </div>
-          </div>
+            <div className="col-md-4 col-md-offset-2">
+              <div className="input-group">
+                <span className="input-group-addon">Start Date</span>
+                <DatePicker
+                  selected={this.state.startTime}
+                  onChange={this.handleStartDateChange}
+                  mindate={new Date("2012-01-01")}
+                  maxdate={new Date()}
+                  dateformat="YYYY-MM-DD"
+                />
+              </div>
+            </div>
           </div>
         </div>
         
-        <div id="canvasContainer">
-          <canvas ref="chart" id="chart" width="400" height="140"></canvas>
-        </div>
+        <Chart data={this.state.chartData} />
       </div>
     );
   }
+  componentDidMount() {
+    this.getBitcoinData();
+    this.getSearchData();
+  }
+  
+  // Input handlers
   handleKeyword(keyword) {
     // Cancel any pending timeouts
     if (this.timeout) {
@@ -163,14 +107,120 @@ class App extends Component {
     // Set new timeout
     this.timeout = setTimeout(() => {
       //console.log(keyword);
-      this.getSearchData(keyword);
+      this.searchKeyword = keyword;
+      this.getSearchData();
     },850);
+  }
+  handleStartDateChange(moment) {
+    this.setState({startTime: moment},() => {
+      this.getBitcoinData();
+      this.getSearchData();
+    });
+  }
+  
+  // Chart Data and Update
+  // **** These should probably be pushed into the Chart component, it should handle fetching it's own data
+  //      Then we can pass in startDate to the component, but only update it on data change
+  getBitcoinData() {
+    // Data from Coindesk (see /routes/coindesk.js)
+    // Add /api/ to start of endpoint for production    
+    var endpoint = "/coindesk/" + this.state.startTime.valueOf() + "-" + this.endTime.valueOf();
+    
+    fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+      //console.log(data);
+      this.bitcoinLabels = [];
+      this.bitcoinData = [];
+      for (var key in data.bpi) {
+        this.bitcoinLabels.push(key);
+        this.bitcoinData.push(data.bpi[key]);
+      }
+    })
+    .catch(err => console.log("Fetch error: " + err))
+  }
+  getSearchData() {
+    // Data from Google Trends (see /routes/googletrendsapi.js)
+    // Add /api/ to start of endpoint for production
+    var endpoint = "/googletrendsapi/" + encodeURIComponent(this.searchKeyword) + "/" + this.state.startTime.valueOf() + "-" + this.endTime.valueOf();
+    
+    fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+      //console.log(data);
+      this.searchLabels = [];
+      this.searchData = [];
+      data.default.timelineData.map(item => {
+        let sunday = new Date(item.formattedAxisTime);
+        for (let d = 0; d < 7; d++) {
+          let day = new Date(sunday);
+          day.setDate(day.getDate() + d);
+          this.searchLabels.push(dateformat(day,"isoDate"));
+          this.searchData.push(item.value[0]);
+        }
+      });
+      //console.log(this.searchLabels);
+      this.updateChartData();
+    })
+    .catch(err => console.log("Fetch error: " + err))
+  }
+  updateChartData() {
+    let chartData = this.state.chartData;
+    chartData.labels = this.bitcoinLabels; // Use the dates from coindesk data as a base
+    chartData.datasets[0].data = this.bitcoinData;
+    chartData.datasets[1].label = '"' + this.searchKeyword + '"';
+    chartData.datasets[1].data = [];
+    
+    // Find values for each of the dates from coindesk data
+    this.bitcoinLabels.map(date => {
+      let match = false;
+      for (let i = 0; i < this.searchLabels.length; i++) {
+        if (this.searchLabels[i] == date) {
+          chartData.datasets[1].data.push(this.searchData[i]);
+          match = true;
+          break;
+        }
+      }
+      if (!match) { chartData.datasets[1].data.push(null); }
+    });
+    this.setState({ chartData: chartData });
+  }
+}
+
+class Chart extends Component {
+  constructor(props) {
+    super(props);
+    
+    // Chart.js defaults
+    ChartJs.defaults.global.elements.point.radius = 1;
+    ChartJs.scaleService.updateScaleDefaults('logarithmic', {
+      ticks: {
+        callback: function(tick, index, ticks) {
+          return tick.toLocaleString();
+        }
+      }
+    });
+  }
+  shouldComponentUpdate(nextProps) {
+    // How do I get this thing to update initially, but not later on parent's state.startDate change?
+    // This component should only update when the data changes
+    console.log(this.props.data.datasets[1].data);
+    console.log(nextProps.data.datasets[1].data);
+    
+    return true;
+  }
+  render() {
+    return(
+      <div id="canvasContainer">
+        <canvas ref="chart" id="chart" width="400" height="140"></canvas>
+      </div>
+    );
   }
   componentDidUpdate() {
     if (this.chart) { this.chart.destroy(); }
-    this.chart = new Chart(this.refs["chart"], {
+    this.chart = new ChartJs(this.refs["chart"], {
       type: 'line',
-      data: this.state.chartData,
+      data: this.props.data,
       options: {
         scales: { 
           yAxes: [
