850212d73ddb7a849628edb467f43d1e6a5649d9
gerbus <gerbus3@gmail.com>
2017-11-13 09:51:16 -0800

    got start date working, but chart refreshes twice

--- routes/coindesk.js
+++ routes/coindesk.js
@@ -3,11 +3,11 @@ var router = express.Router();
 var dateformat = require('dateformat');
 var fetch = require('node-fetch');
 
-
 // Fetch Coindesk data and pass to front-end
 router.get("/:startTime-:endTime", function(req, res, next) {
   let startTime = new Date(parseInt(req.params.startTime));
   let endTime = new Date(parseInt(req.params.endTime));
+  console.log(startTime);
   
   const endpoint = "https://api.coindesk.com/v1/bpi/historical/close.json?start=" + dateformat(startTime,"isoDate") + "&end=" + dateformat(endTime,"isoDate");
   
